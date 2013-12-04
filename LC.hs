fc5bdd171159c918b7a153003172ce901d403d10
fmdkdd <fmdkdd@gmail.com>
2013-12-04 16:25:42 +0100

    Signal instrumented code with `i13n` (instrumentation) comment

--- LC.hs
+++ LC.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
+-- i13n stands for 'instrumentation'.  Ideally, all i13n changes to
+-- the code should be localized, and could be separately compiled and
+-- activated.
+
 import Control.Monad.State
 import Control.Monad.Identity
 import AOP.Default
@@ -9,9 +13,9 @@ import Debug.Trace
 
 type Name      = String
 type Address   = Int
-type Principal = (Int, Bool)
-type ProgCounter = [Principal]
- 
+type Principal = (Int, Bool) -- i13n
+type ProgCounter = [Principal] -- i13n
+
 data Term = Bot
           | Con Int
           | Boolean Bool
@@ -23,7 +27,7 @@ data Term = Bot
           | Assign Term Term
           | If Term Term Term
           | Let Name Term Term
-          | Facet Principal Term Term
+          | Facet Principal Term Term -- i13n
 
 data Value = Error String
            | Bottom
@@ -31,10 +35,10 @@ data Value = Error String
            | BoolConstant Bool
            | Address Int
            | Closure (Value -> M Value)
-           | FacetV Principal Value Value
+           | FacetV Principal Value Value -- i13n
    deriving Typeable
 
-
+-- i13n
 createFacetValue :: ProgCounter -> Value -> Value -> Value
 createFacetValue [] vH vL                 = vH
 createFacetValue (k@(_,True):rest)  vH vL = FacetV k (createFacetValue rest vH vL) vL
@@ -46,8 +50,6 @@ createFacetValue (k@(_,False):rest) vH vL = FacetV k vL (createFacetValue rest v
 
 -- class InterpC t where
 --   interpC :: Term -> Environment -> MValue
- 
-
 
 instance Eq Value where
   Error s1 == Error s2               = s1 == s2
@@ -56,7 +58,7 @@ instance Eq Value where
   BoolConstant b1 == BoolConstant b2 = b1 == b2
   Address i1 == Address i2           = i1 == i2
   Closure f == Closure g             = False
-  FacetV p v1 v2 == FacetV q w1 w2   = p == q && v1 == w1 && v2 == w2
+  FacetV p v1 v2 == FacetV q w1 w2   = p == q && v1 == w1 && v2 == w2 -- i13n
 
 instance Show Value where
   show (Error s)        = "<Error> " ++ show s
@@ -65,17 +67,19 @@ instance Show Value where
   show (BoolConstant b) = show b
   show (Address i)      = "<address> " ++ show i
   show (Closure f)      = "<function>"
-  show (FacetV p v1 v2) = "(FacetV: " ++ show p ++ ", " ++ show v1 ++ ", " ++ show v2 ++ ")"
-  
+  show (FacetV p v1 v2) = "(FacetV: " ++ show p ++ ", " ++ show v1 ++ ", " ++ show v2 ++ ")" -- i13n
+
 type Environment = [(Name, Value)]
 type Store = [(Address, Value)]
 
+-- i13n
 type M a = AOT (StateT ProgCounter (StateT Store Identity)) a
 
+-- i13n
 runM :: M Value -> ProgCounter -> Store -> ((Value, ProgCounter), Store)
 runM m pc s = runIdentity (runStateT (runStateT (runAOT prog) pc) s)
- where prog = do deploy (aspect (pcCall goIf) goIfAdv)
-                 deploy (aspect (pcCall goRef) goRefAdv)                 
+ where prog = do deploy (aspect (pcCall goIf) goIfAdv) -- i13n
+                 deploy (aspect (pcCall goRef) goRefAdv) -- i13n
                  m
 
 goIf :: (Environment, Term) -> M Value
@@ -86,6 +90,7 @@ goIf (e, (If cond thn els)) =
        (BoolConstant b) -> if b then interp thn e
                                 else interp els e
 
+-- i13n
 goIfAdv proceed args@(e, (If cond thn els)) =
   do b <- interp cond e
      case b of
@@ -103,6 +108,7 @@ goRef (e,(Ref t)) =
      (lift . lift . put) ((addr,v):store)
      return (Address addr)
 
+-- i13n
 goRefAdv proceed args@(e, (Ref t)) =
   do result@(Address addr) <- proceed args
      progCounter <- get
@@ -113,7 +119,7 @@ goRefAdv proceed args@(e, (Ref t)) =
                            (createFacetValue progCounter v Bottom)
                            store)
      return result
-     
+
 interp :: Term -> Environment -> M Value
 interp Bot e         = return Bottom
 interp (Con i) e     = return (Constant i)
@@ -121,17 +127,19 @@ interp (Boolean b) e = return (BoolConstant b)
 interp (Var x) e     = return (envLookup x e)
 interp (Lam x v) e   = return (Closure (\a -> interp v ((x,a):e)))
 
+-- i13n
 -- should yield terms rather than values, see rule F-IF-SPLIT
 interp (Facet p t1 t2) e =
   do vH <- interp t1 e
      vL <- interp t2 e
      return (FacetV p vH vL)
-          
-interp (App t u) e = 
+
+interp (App t u) e =
   do f <- interp t e
      a <- interp u e
      apply f a
-                          
+
+-- i13n
 interp expr@(Ref t) e = goRef # (e, expr)
 
 interp (Deref t) e =
@@ -143,6 +151,7 @@ interp (Assign l r) e =
      rv <- interp r e
      assign lv rv
 
+-- i13n
 interp expr@(If cond thn els) e = goIf # (e, expr)
 
 interp (Let id namedExpr body) e = interp (App (Lam id body) namedExpr) e
@@ -150,26 +159,26 @@ interp (Let id namedExpr body) e = interp (App (Lam id body) namedExpr) e
 envLookup :: Name -> Environment -> Value
 envLookup x [] = (Error ("unbound " ++ show x))
 envLookup x ((y,b):e) = if x == y then b else envLookup x e
- 
+
 storeLookup :: Address -> Store -> Value
 storeLookup a [] = (Error ("not in store " ++ show a))
 storeLookup a ((b,v):s) = if a == b then v else storeLookup a s
- 
+
 storeReplace :: Address -> Value -> Store -> Store
 storeReplace a v [] = []
 storeReplace a v ((b,w):s) = if a == b then ((a,v):s) else storeReplace a v s
- 
+
 apply :: Value -> Value -> M Value
 apply (Closure f) a = f a
 apply Bottom _      = return Bottom
 apply _ _           = return (Error "apply: not a closure or bottom")
- 
+
 deref :: Value -> M Value
 deref Bottom      = return Bottom
 deref (Address a) = do store <- lift $ lift $ get
                        return (storeLookup a store)
 deref _           = return (Error "deref: not an address or bottom")
- 
+
 assign :: Value -> Value -> M Value
 assign Bottom _      = return Bottom
 assign (Address a) v = do store <- lift $ lift $ get
@@ -179,15 +188,17 @@ assign (Address a) v = do store <- lift $ lift $ get
 assign _ _ = return (Error "assign: not an address or bottom")
 
 -- use implicit parameters??
-             
+
 test :: Term -> Environment -> ProgCounter -> Store -> String
 test t env pc store = show (runM (interp t env) pc store)
 
 testDefault t = test t [] [] []
 
+-- i13n
 testWithPC :: Term -> ProgCounter -> String
 testWithPC t pc = show (runM (interp t []) pc [])
 
+-- i13n
 runWithPC t pc = runM (interp t []) pc []
 
 -- Tests, we skip them for now!
@@ -203,7 +214,3 @@ facetTest1 = (If (Facet (1,True) (Boolean True) (Boolean False)) (Con 42) (Con 2
 
 -- assert facetTest1 ==
 facetTest2 = (Ref (Con 843))
-
-
-  
-
