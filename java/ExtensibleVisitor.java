f34b90549f9ddd1d511363ab2210b3cff5fcd333
fmdkdd <fmdkdd@gmail.com>
2016-08-10 18:48:10 +0200

    Add term

--- java/ExtensibleVisitor.java
+++ java/ExtensibleVisitor.java
@@ -26,5 +26,25 @@ class Test {
   public static void main(String args[]) {
     Term e1 = new Plus(new Constant(1), new Constant(2));
     System.out.println(e1.accept(new Eval()));
+
+    Term e2 = new Plus(new Mult(new Constant(1), new Constant(2)), new Constant(1));
+    System.out.println(e2.accept(new EvalForMult()));
+  }
+}
+
+/* Add Mult */
+class Mult extends Term {
+  Term a, b;
+  Mult(Term a, Term b) { this.a = a; this.b = b; }
+  <T> T accept(Visitor<T> v) { return ((VisitorForMult<T>)v).forMult(this); }
+}
+
+interface VisitorForMult<T> extends Visitor<T> {
+  T forMult(Mult m);
+}
+
+class EvalForMult extends Eval implements VisitorForMult<Integer> {
+  public Integer forMult(Mult m) {
+    return m.a.accept(this) * m.b.accept(this);
   }
 }
